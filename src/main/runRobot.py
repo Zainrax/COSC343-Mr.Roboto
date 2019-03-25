1bcf550f0fc8fc38e2e0043a8de3979e3855555b
green0range <william.satterthwaite1008@gmail.com>
2019-03-26 11:24:03 +1300

    commented

--- src/main/runRobot.py
+++ src/main/runRobot.py
@@ -3,73 +3,106 @@ import math
 import time
 from threading import Thread
 
-from ev3dev2 import *
-from ev3dev2.button import Button
 from ev3dev2.motor import MoveTank, OUTPUT_B, OUTPUT_C
-from ev3dev2.sensor.lego import ColorSensor, TouchSensor, UltrasonicSensor
+from ev3dev2.sensor.lego import ColorSensor, UltrasonicSensor
 from ev3dev2.sound import Sound
 
 
 class Robot:
+    """Robot class structure for running a course.
+
+    """
     def __init__(self):
+        """Initialize robot's sensors and define distances and color ranges.
+        """
         self.tank_pair = MoveTank(OUTPUT_B, OUTPUT_C)
         self.color_sensor = ColorSensor()
         self.ultrasonic = UltrasonicSensor()
         self.sound = Sound()
         self.tile_length = 200
+        self.sensor_dist = 86
         # Ranges for color intensities
         self.black_range = range(0, 30)
         self.white_range = range(30, 100)
-        self.sensor_dist = 86
 
     def move_degrees(self, degrees, speed=20):
+        """ Turn wheels a set number of degrees.
+
+        :param degrees: Degrees to turn the wheels.
+        :param speed: Power with which wheels turn.
+        """
         if degrees >= 0:
             self.tank_pair.on_for_degrees(left_speed=speed, right_speed=speed, degrees=degrees)
         else:
             self.tank_pair.on_for_degrees(left_speed=-speed, right_speed=-speed, degrees=-degrees)
 
-    def steer_on(self, steering, speed=20):
-        if steering >= 0:
-            self.tank_pair.on(left_speed=speed, right_speed=0)
-        else:
-            self.tank_pair.on(left_speed=0, right_speed=speed)
-
     def on(self, speed=20):
+        """ Turn the motors on.
+
+        :param speed: Power with which wheels turn.
+        """
         self.tank_pair.on(left_speed=speed, right_speed=speed)
 
     def off(self):
+        """Turn the motors off.
+        """
         self.tank_pair.off()
 
     def on_white(self):
+        """Return whether the color sensor senses white.
+
+        :return: Whether the color sensor senses white.
+        """
         return self.color_sensor.reflected_light_intensity in self.white_range
 
     def on_black(self):
+        """Return whether the color sensor senses black.
+
+        :return: Whether the color sensor senses black.
+        """
         return self.color_sensor.reflected_light_intensity in self.black_range
 
     def skip_white(self, speed=20):
+        """Drive while robot senses white.
+
+        :param speed: Power with which wheels turn
+        """
         self.on(speed=speed)
         while self.on_white():
             pass
         self.off()
 
     def skip_black(self, speed=20):
+        """Drive while robot senses black.
+
+        :param speed: Power with which wheels turn.
+        """
         self.on(speed=speed)
         while self.on_black():
             pass
         self.off()
 
     def turn(self, degrees, speed=20):
+        """Turn robot a number of degrees.
+
+        :param degrees: Degrees to rotate the robot.
+        :param speed: Power with which wheels turn.
+        """
         if degrees >= 0:
             self.tank_pair.on_for_degrees(left_speed=speed, right_speed=-speed, degrees=degrees * 1.987)
         else:
             self.tank_pair.on_for_degrees(left_speed=-speed, right_speed=speed, degrees=-degrees * 1.987)
 
     def run(self):
+        """Run robot through course.
+        """
         self.initialize_start()
         self.count_tiles()
         self.bump_tower()
 
     def initialize_start(self):
+        """Move robot from start to first black tile.
+        """
         self.move_degrees(80)
         self.skip_black()
         self.skip_white()
@@ -79,21 +112,9 @@ class Robot:
         self.skip_black(speed=-20)
         self.skip_white()
 
-    def get_angle_from_color(self, left2, left1, right1, right2):
-        num = 0
-        if not left2:
-            num += 8
-        if not left1:
-            num += 4
-        if not right1:
-            num += 2
-        if not right2:
-            num += 1
-
-        angle_list = [0, -7, 3.5, 7, -3.5, -7, 0, 3.5, 7, 0, 7, 7, -7, -7, -3.5, 0]
-        return angle_list[num]
-
     def count_tiles(self):
+        """Move across 15 black tiles while counting
+        """
         tile_count = 0
         turn_angle = 0
         prev_turn_angle = 0
@@ -104,25 +125,25 @@ class Robot:
             self.move_degrees(135)
             
             self.turn(90)
-            right_is_white_1 = self.on_white()
+            right_is_black_1 = self.on_black()
             self.move_degrees(40)
-            right_is_white_2 = self.on_white()
+            right_is_black_2 = self.on_black()
             self.move_degrees(-40)
             self.turn(-180)
-            left_is_white_1 = self.on_white()
+            left_is_black_1 = self.on_black()
             self.move_degrees(40)
-            left_is_white_2 = self.on_white()
+            left_is_black_2 = self.on_black()
             self.move_degrees(-40)
             self.turn(90)
 
-            turn_angle = self.get_angle_from_color(left_is_white_2, left_is_white_1, right_is_white_1, right_is_white_2)
+            turn_angle = self.get_angle_from_color(left_is_black_2, left_is_black_1, right_is_black_1, right_is_black_2)
             if prev_turn_angle > 0 and turn_angle > 0:
                 turn_angle = 3.5
             elif prev_turn_angle < 0 and turn_angle < 0:
                 turn_angle = -3.5
             elif turn_angle == 0:
-                self.turn(-prev_turn_angle)
-            self.turn(turn_angle)
+                self.turn(-prev_turn_angle, speed=8)
+            self.turn(turn_angle, speed=8)
             prev_turn_angle = turn_angle
             
             self.skip_black()
@@ -132,18 +153,43 @@ class Robot:
         self.sound.play_tone(100 + (50 * tile_count), 0.5)
         self.move_degrees(self.tile_length)
 
-    def cm_to_degrees(self, cm):
-        return (360/(6*math.pi))*cm
+    def get_angle_from_color(self, left2, left1, right1, right2):
+        """Get adjustment angle from color readings.
 
-    def turn_360(self):
+        :param left2: Whether left outer color is black.
+        :param left1: Whether left inner color is black.
+        :param right1: Whether right inner color is black.
+        :param right2: Whether right outer color is black.
+        :return: Angle to adjust by.
+        """
+        # treat color booleans as a binary number to get an index in angle_list
+        index = 0
+        if left2:
+            index += 8
+        if left1:
+            index += 4
+        if right1:
+            index += 2
+        if right2:
+            index += 1
+
+        angle_list = [0, -7, 3.5, 7, -3.5, -7, 0, 3.5, 7, 0, 7, 7, -7, -7, -3.5, 0]
+        return angle_list[index]
+
+    def turn_370(self):
+        """Turn robot 370 degrees.
+        """
         self.turn(degrees=370)
 
-    # point robot towards tower; return distance in cm
     def search_for_tower(self):
+        """Point robot at tower.
+
+        :return: Distance to tower.
+        """
         min_dist = 255
         start_time = time.time()
         min_time = start_time
-        turn_thread = Thread(target=self.turn_360)
+        turn_thread = Thread(target=self.turn_370)
         turn_thread.start()
         while turn_thread.is_alive():
             cur_dist = self.ultrasonic.distance_centimeters
@@ -155,7 +201,17 @@ class Robot:
         self.turn(degrees=370*(min_time-start_time)/(end_time-start_time))
         return min_dist
 
+    def cm_to_degrees(self, cm):
+        """Convert cm to degrees.
+
+        :param cm: Amount to convert.
+        :return: Amount in degrees.
+        """
+        return (360/(6*math.pi))*cm
+
     def bump_tower(self):
+        """Find tower and knock it off its base.
+        """
         self.turn(degrees=90)
         self.move_degrees(self.tile_length * 18)
         
@@ -184,7 +240,6 @@ if __name__ == "__main__":
     try:
         r = Robot()
         r.run()
-
     except:
         import traceback
         exc_type, exc_value, exc_traceback = sys.exc_info()
